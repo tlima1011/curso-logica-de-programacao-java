7b3fb4e36875d23c5e7c048699814c62841dfd1f
tlima1011 <thiagolima1011@gmail.com>
2024-07-06 14:48:37 -0300

    URI1012 - AREAS

--- src/uri1012.java
+++ src/uri1012.java
@@ -6,16 +6,27 @@ public class uri1012 {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US); 
-		Scanner sc = new Scanner(System.in); 
+		Scanner ler = new Scanner(System.in); 
 		
-		double π = 3.14159;
+		double pi = 3.14159;
 		
-		double r = sc.nextDouble(); 
-		double a = π * Math.pow(r,2); 
-			
-		System.out.printf("A=%.4f%n",a);
+		double a = ler.nextDouble(); 
+		double b = ler.nextDouble(); 
+		double c = ler.nextDouble(); 
 		
-		sc.close();
+		double triangulo = (a * c) / 2.0; 
+		double circulo = pi * Math.pow(c, 2);
+		double trapezio = (a + b) * c / 2.0;
+		double quadrado = b * b;
+		double retangulo = a * b; 
+		
+		System.out.printf("TRIANGULO: %.3f%n",triangulo);
+		System.out.printf("CIRCULO: %.3f%n",circulo);
+		System.out.printf("TRAPEZIO: %.3f%n",trapezio); 
+		System.out.printf("QUADRADO: %.3f%n",quadrado);  
+		System.out.printf("RETANGULO: %.3f%n",retangulo);
+		
+		ler.close();
 
 	}
 
