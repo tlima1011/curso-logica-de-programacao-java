4c88e284dbbee95a0cacf073f61b5d2ebc076622
tlima1011 <thiagolima1011@gmail.com>
2024-07-06 15:03:56 -0300

    URI1003

--- src/uri1003.java
+++ src/uri1003.java
@@ -1,9 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class uri1003 {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in); 
 		
+		int a = sc.nextInt(); 
+		int b = sc.nextInt(); 
+		int s = a + b; 
+		System.out.println("SOMA = "+s);
+		
+		sc.close();
 		
 	}
 
