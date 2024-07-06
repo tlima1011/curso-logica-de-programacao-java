561776523eba13331ded8a7cbb360f7edbc21c8e
tlima1011 <thiagolima1011@gmail.com>
2024-07-06 14:24:37 -0300

    URI 1011 Resolvido

---
+++ src/uri1011.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class uri1011 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		int a = sc.nextInt(); 
+		int b = sc.nextInt(); 
+		
+		int x = a + b; 
+		System.out.println("X = " + x);
+		
+		sc.close();
+
+	}
+}
