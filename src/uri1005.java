b34f5f9d9d8403ab7ecf45f1ad54f96569aa9bab
tlima1011 <thiagolima1011@gmail.com>
2024-07-06 14:39:25 -0300

    URI1005

---
+++ src/uri1005.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1005 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US); 
+		Scanner sc = new Scanner(System.in); 
+		
+		double A = sc.nextDouble();
+		double B = sc.nextDouble(); 
+		
+		double MEDIA = (A * 3.5 + B * 7.5) / 11.0;
+		
+		System.out.printf("MEDIA = %.5f%n", MEDIA);
+
+	}
+
+}
