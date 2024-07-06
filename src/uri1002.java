56222e9886da17218cb257b57d7713ec15ad4dd0
tlima1011 <thiagolima1011@gmail.com>
2024-07-06 14:36:24 -0300

    URI1002 - CORRIGIDO

---
+++ src/uri1002.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1002 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US); 
+		Scanner sc = new Scanner(System.in); 
+		
+		double π = 3.14159;
+		
+		double r = sc.nextDouble(); 
+		double a = π * Math.pow(r,2); 
+			
+		System.out.printf("A=%.4f%n",a);
+		
+		sc.close();
+
+	}
+
+}
