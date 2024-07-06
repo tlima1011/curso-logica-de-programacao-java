93287deefd47d2f66b442d28721ae93ae32289c2
tlima1011 <thiagolima1011@gmail.com>
2024-07-06 14:34:26 -0300

    URI1001 - CORRIGIDO

---
+++ src/uri1001.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class uri1001 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		int a = sc.nextInt(); 
+		int b = sc.nextInt(); 
+		
+		int x = a + b; 
+		System.out.println("X = " + x);
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/uri1003.java
@@ -0,0 +1,10 @@
+
+public class uri1003 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
