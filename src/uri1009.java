5e93abaf74877f59b286110efccbad0d0790c68c
tlima1011 <thiagolima1011@gmail.com>
2024-07-06 14:44:40 -0300

    URI1009

---
+++ src/uri1009.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1009 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US); 
+		Scanner sc = new Scanner(System.in); 
+		
+		String nome = sc.next(); 
+		double salario = sc.nextDouble(); 
+		double totalVendas = sc.nextDouble(); 
+		double total = salario + (totalVendas  *  0.15);
+		
+		System.out.printf("TOTAL = R$ %.2f%n",total);
+		
+		sc.close();
+
+	}
+
+}
