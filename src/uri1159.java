10353751ab94711fa86d2331f9a5d3fde2734a4d
tlima1011 <thiagolima1011@gmail.com>
2024-07-14 15:23:03 -0300

    URI1159

---
+++ src/uri1159.java
@@ -0,0 +1,13 @@
+
+public class uri1159 {
+
+	public static void main(String[] args) {
+		
+		int i = 2; 
+		
+		while(i <= 100) { 
+			System.out.print(i % 2 == 0? i + "\n" : "");
+			i = i + 1;
+		}
+	}
+}
