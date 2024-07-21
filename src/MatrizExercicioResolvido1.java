3013c4732b316aea13d9c94e9006381b59e9ad01
tlima1011 <thiagolima1011@gmail.com>
2024-07-21 12:05:09 -0300

    MatrizExercicioResolvido1 - Resolvido

---
+++ src/MatrizExercicioResolvido1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MatrizExercicioResolvido1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		int n = sc.nextInt(); 
+		int mat[][] = new int[n][n]; 
+		int negativos = 0; 
+		
+		for(int i = 0; i < n;i++) { 
+			for(int j = 0; j < n;j++) { 
+				mat[i][j] = sc.nextInt();
+				if(mat[i][j] < 0) {
+					negativos++;
+				}
+			}
+		}
+		System.out.println("DIAGONAL PRINCIPAL: ");
+		for(int i = 0; i < n;i++) { 
+			System.out.printf("%d ", mat[i][i]);
+		}
+		
+		System.out.println("\nQUANTIDADE DE NEGATIVOS = "+ negativos);
+		
+		sc.close();	
+	}
+}
